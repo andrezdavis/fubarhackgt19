fb7c703f53710a696b743aa55397d067ec531e61
andrezdavis <adavis220@student.gsu.edu>
2019-10-26 04:54:02 -0400

    initial commit

--- fubarhackgt19app/routes/index.js
+++ fubarhackgt19app/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var request = require('request');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// request.get('')
 
 module.exports = router;
